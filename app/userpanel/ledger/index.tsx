98ccbb79c77375dfa60310f28db009a22bfddba6
Sabareesh001 <hatrickda@gmail.com>
2025-09-12 16:24:07 +0530

    completed monthly income feature

--- app/userpanel/ledger/index.tsx
+++ app/userpanel/ledger/index.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@/components/atoms/button/button";
 import { SectionHeading } from "@/components/atoms/heading/heading";
 import { Label } from "@/components/atoms/label/label";
 import { TextField } from "@/components/atoms/textField/textField";
 import { useTheme } from "@/theme/themeProvider";
+import { BASE_URL } from "@/utils/apiHost";
 import { GetCurrencySymbol } from "@/utils/currency";
+import AntDesign from "@expo/vector-icons/AntDesign";
 import { useAsyncStorage } from "@react-native-async-storage/async-storage";
+import axios, { Axios, AxiosResponse } from "axios";
 import getSymbolFromCurrency from "currency-symbol-map";
 import { useEffect, useState } from "react";
 import { StyleSheet, View } from "react-native";
@@ -14,17 +18,55 @@ export default function Ledger(){
 
     const currency = useAsyncStorage("currency")
 
-
+    const [monthyIncome,setMonthlyIncome] = useState(0.0)
+    const [originalMonthlyIncome,setOriginalMonthlyIncome] = useState(0.0)
     const [currencySymbol,setCurrencySymbol] = useState("")
+    const [incomeSaveLoading,setIncomeSaveLoading] = useState(false)
 
     const styles = StyleSheet.create({
         pageContainer : {
             padding: theme?.paddings.page,
-            gap:theme?.gaps.form
+            gap:theme?.gaps.form,
+        },
+        incomeContainer : {
+            flexDirection:'row',
+            gap:10,
+            
         }
     })
 
+    type IncomeResponse = {
+        message:string,
+        data:number
+    }
+
+    const fetchMonthlyIncome = ()=>{
+
+        axios.get(`${BASE_URL}/api/v1/income`).then((res:AxiosResponse<IncomeResponse>)=>{
+            setMonthlyIncome(res.data.data)
+            setOriginalMonthlyIncome(res.data.data)
+        }).catch((err)=>{ 
+            console.log(err.response)
+        })
+
+    }
+
+    const saveNewIncome = ()=>{
+        setIncomeSaveLoading(true)
+        axios.patch(`${BASE_URL}/api/v1/income/`,{income:monthyIncome}).then((res)=>{
+            console.log(res)
+        }).catch((err)=>{
+            console.log(err)
+        }).finally(()=>{
+            setIncomeSaveLoading(false)
+        })
+    }
+    
     useEffect(()=>{
+        fetchMonthlyIncome()
+    },[])
+ 
+    useEffect(()=>{ 
 
         const setCurrSymbol = async()=>{
             const symbol = await GetCurrencySymbol()
@@ -32,8 +74,11 @@ export default function Ledger(){
         }
 
         setCurrSymbol()
-
+        
     },[currency])
+    
+    
+
 
 
      return(
@@ -41,9 +86,16 @@ export default function Ledger(){
             <Label>
                 Monthly Income
             </Label>
-            <TextField value={`${currencySymbol} 1234`}>
-
+            <View style={styles.incomeContainer}>
+                <View style={{flex:9}}>
+            <TextField keyboardType="number-pad" inputMode="numeric" onChangeText={(e)=>{setMonthlyIncome(()=>{
+                const converted = Number(e.slice(1))
+                return isNaN(converted)?0:converted
+            })}} editable value={`${currencySymbol} ${monthyIncome}`}>
             </TextField>
+                </View>
+            <Button onPress={saveNewIncome} loading={incomeSaveLoading} containerStyle={{flex:2}} disabled={originalMonthlyIncome==monthyIncome}  icon={<AntDesign  name="save"  size={20} color={theme?.colors.secondary}/>}/>
+            </View>
         </View>
      )
 }
